0bd72b1660f3d45c4a06cc98c7645698fca5cc45
sam rat <sam.rat22021@gmail.com>
2022-02-16 04:07:49 +0545

    Update

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 
 import os
 import telebot
+
+bot.polling
 TOKEN = '5120823518:AAFYN0Y_9YTislvL0pklBfxxm9PYjOXj8dk'
       bot = telebot.TeleBot(TOKEN)
           @bot.message_handler(commands=['start', 'help'])
