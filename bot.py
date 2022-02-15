0986fe6c068cf34ad48bafcbcf30fb004ae6b835
sam rat <sam.rat22021@gmail.com>
2022-02-16 03:28:33 +0545

    first

---
+++ bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 03:01:09 2022
+
+@author: insanesu
+"""
+
+import os
+import telebot
+TOKEN = '5120823518:AAFYN0Y_9YTislvL0pklBfxxm9PYjOXj8dk'
+      bot = telebot.TeleBot(TOKEN)
+          @bot.message_handler(commands=['start', 'help'])
+          def command_help(message):
+              bot.reply_to(message, "Hello, did someone call for help?")
