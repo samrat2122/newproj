c0d4de7b0d43c049205d5b37b9f658a17b8f380b
sam rat <sam.rat22021@gmail.com>
2022-02-16 03:32:01 +0545

    bot.py Update

--- bot.py
+++ bot.py
@@ -12,3 +12,5 @@ TOKEN = '5120823518:AAFYN0Y_9YTislvL0pklBfxxm9PYjOXj8dk'
           @bot.message_handler(commands=['start', 'help'])
           def command_help(message):
               bot.reply_to(message, "Hello, did someone call for help?")
+
+bot.polling
